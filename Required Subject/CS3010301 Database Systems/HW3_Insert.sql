6fd649c0d4d71882138a63a8f69d5385706d33ca
William Ren <Williamren97@gmail.com>
2019-06-11 22:07:10 +0800

    Add files via upload

---
+++ Required Subject/CS3010301 Database Systems/HW3_Insert.sql
@@ -0,0 +1,99 @@
+/*第一题*/
+/*COURSE*/
+INSERT INTO `COURSE` (`Course_name`, `Course_number`, `Credit_hours`, `Department`) VALUES
+('Intro to Computer Science', 'CS1310', 4, 'CS'),
+('Data Structures', 'CS3320', 4, 'CS'),
+('Database', 'CS3380', 3, 'CS'),
+('Financial Accounting', 'fac4390', 5, 'BUSI'),
+('Discrete Mathematics', 'MATH2410', 3, 'MATH');
+
+
+/*SECTION*/
+INSERT INTO `SECTION` (`Section_identifier`, `Course_number`, `Semester`, `Year`, `Instructor`) VALUES
+(85, 'MATH2410', 'Fall', '07', 'King'),
+(92, 'CS1310', 'Fall', '07', 'Anderson'),
+(102, 'CS3320', 'Spring', '08', 'Knuth'),
+(112, 'MATH2410', 'Fall', '08', 'Chang'),
+(119, 'CS1310', 'Fall', '08', 'Anderson'),
+(135, 'CS3380', 'Fall', '08', 'Stone');
+
+/*PREREQUISITE*/
+INSERT INTO `PREREQUISITE` (`Course_number`, `Prerequisite_number`) VALUES
+('CS3320', 'CS1310'),
+('CS3380', 'CS3320'),
+('CS3380', 'MATH2410');
+
+/*GRADE_REPORT*/
+INSERT INTO `GRADE_REPORT` (`Student_number`, `Section_identifier`, `Grade`) VALUES
+(8, 85, 'A'),
+(8, 92, 'A'),
+(8, 102, 'B'),
+(8, 135, 'A'),
+(17, 112, 'B'),
+(17, 119, 'C');
+
+
+/*第二题*/
+/*DEPARTMENT*/
+INSERT INTO `DEPARTMENT` (`Dname`, `Dnumber`, `Mgr_ssn`, `Mgr_start_date`) VALUES
+('Headquarters', 1, '888665555', '1981-06-19'),
+('Information Technology', 2, '987987987', '2007-10-01'),
+('Administration', 4, '987654321', '1995-01-01'),
+('Research', 5, '333445555', '1988-05-22');
+
+/*EMPLOYEE*/
+INSERT INTO `EMPLOYEE` (`Fname`, `Minit`, `Lname`, `Ssn`, `Bdate`, `Address`, `Sex`, `Salary`, `Super_ssn`, `Dno`) VALUES
+('John', 'B', 'Smith', '123456789', '1965-01-09', '731 Fondren, Houston, TX', 'M', 30000, '333445555', 5),
+('Franklin', 'T', 'Wong', '333445555', '1955-12-08', '638 Voss, Houston, TX', 'M', 40000, '888665555', 5),
+('Alicia', 'J', 'Zelaya', '999887777', '1968-01-19', '3321 Castle, Spring, TX', 'F', 25000, '987654321', 4),
+('Jennifer', 'S', 'Wallace', '987654321', '1941-06-20', '291 Berry, Bellaire, TX', 'F', 43000, '888665555', 4),
+('Ramesh', 'K', 'Narayan', '666884444', '1962-09-15', '975 Fire Oak, Humble, TX', 'M', 38000, '333445555', 5),
+('Joyce', 'A', 'English', '453453453', '1972-07-31', '5631 Rice, Houston, TX', 'F', 25000, '333445555', 5),
+('Ahmad', 'V', 'Jabbar', '987987987', '1969-03-29', '980 Dallas, Houston, TX', 'M', 25000, '987654321', 4),
+('James', 'E', 'Borg', '888665555', '1937-11-10', '450 Stone, Houston, TX', 'M', 55000, '', 1);
+
+/*DEPT_LOCATION*/
+INSERT INTO `DEPT_LOCATIONS` (`Dnumber`, `Dlocation`) VALUES
+(1, 'Houston'),
+(4, 'Stafford'),
+(5, 'Bellaire'),
+(5, 'Sugarland'),
+(5, 'Houston');
+
+/*WORK_ON*/
+INSERT INTO `WORKS_ON` (`Essn`, `Pno`, `Hours`) VALUES
+('123456789', 1, 32.5),
+('123456789', 2, 7.5),
+('666884444', 3, 40.0),
+('453453453', 1, 20.0),
+('453453453', 2, 20.0),
+('333445555', 2, 10.0),
+('333445555', 3, 10.0),
+('333445555', 10, 10.0),
+('333445555', 20, 10.0),
+('999887777', 30, 30.0),
+('999887777', 10, 10.0),
+('987987987', 10, 35.0),
+('987987987', 30, 5.0),
+('987654321', 30, 20.0),
+('987654321', 20, 15.0),
+('888665555', 20, NULL);
+
+/*project*/
+INSERT INTO `PROJECT` (`Pname`, `Pnumber`, `Plocation`, `Dnum`) VALUES
+('ProductX', 1, 'Bellaire', 5),
+('ProductY', 2, 'Sugarland', 5),
+('ProductZ', 3, 'Houston', 5),
+('Computerization', 10, 'Stafford', 4),
+('Reorganization', 20, 'Houston', 1),
+('Newbenefits', 30, 'Stafford', 4);
+
+/*DEPEDENT*/
+INSERT INTO `DEPENDENT` (`Essn`, `Dependent_name`, `Sex`, `Bdate`, `Relationship`) VALUES
+('333445555', 'Alice', 'F', '1986-04-05', 'Daughter'),
+('333445555', 'Theodore', 'M', '1983-10-25', 'Son'),
+('333445555', 'Joy', 'F', '1958-05-03', 'Spouse'),
+('987654321', 'Abner', 'M', '1942-02-28', 'Spouse'),
+('123456789', 'Michael', 'M', '1988-01-04', 'Son'),
+('123456789', 'Alice', 'F', '1988-12-30', 'Daughter'),
+('123456789', 'Elizabeth', 'F', '1967-05-05', 'Spouse');
